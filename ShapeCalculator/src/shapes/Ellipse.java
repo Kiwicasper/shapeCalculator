7d224e95c026bca6525fa56d7d0893bc129106a7
Mathias R. Larsen <mathiaslarsen@10.16.161.195>
2015-10-19 15:34:47 +0200

    Ellipseklassen er nu klar til test

--- ShapeCalculator/src/shapes/Ellipse.java
+++ ShapeCalculator/src/shapes/Ellipse.java
@@ -2,8 +2,19 @@ package src.shapes;
 
 public class Ellipse {
 
+private double width;
+private double height; 
+
 public Ellipse(double width, double height)	{
-	
+	this.width = width;
+	this.height = height; 
 }
-	
+public double getPerimeter(){
+	 double perimeter = 2*Math.PI*Math.sqrt(0.5*(Math.pow(width, 2)+Math.pow(height, 2)));
+	return perimeter;
+}
+public double getArea() {
+double area = Math.PI*width*height;
+return area;	
+}
 }
