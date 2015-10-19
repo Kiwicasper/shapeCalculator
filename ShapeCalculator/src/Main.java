af0fc3ab96cb7f18085e116303989c019a3411a2
Casper Danielsen <kiwicasper@hotmail.com>
2015-10-19 15:39:52 +0200

    Rette småfejl i main

--- ShapeCalculator/src/Main.java
+++ ShapeCalculator/src/Main.java
@@ -1,7 +1,8 @@
 package src;
 import java.util.logging.Logger;
 
-import src.shapes.Circle;
+
+import src.shapes.*;
 
 public class Main {
 
@@ -14,9 +15,9 @@ public class Main {
 		log(t.getArea());
 		log(t.getPerimeter());
 		
-		Ellipse e = new ellipse(23.4,56.4);
-		log(e.getArea); //Major axis, minor axis
-		log(e.getPerimeter); //Width, height
+		Ellipse e = new Ellipse(23.4,56.4);
+		log(e.getArea()); //Major axis, minor axis
+		log(e.getPerimeter()); //Width, height
 		
 		Prism p = new Prism(123.4,new Trapez(123.5,235.1));//Height and base/top
 		log(p.get.SurfaceArea());
