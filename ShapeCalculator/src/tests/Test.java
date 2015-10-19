8229ec583e2a179bd6a7aa267d36e3fa28bc0b7a
Casper Danielsen <kiwicasper@hotmail.com>
2015-10-19 16:20:48 +0200

    Test udbygget

--- ShapeCalculator/src/tests/Test.java
+++ ShapeCalculator/src/tests/Test.java
@@ -6,9 +6,21 @@ public class Test {
 
 	public static void main(String[] args) {
 		Circle c = new Circle(45.3);
+		Ellipse e = new Ellipse(10, 20);
+		Trapez t = new Trapez(12,13,14.3);
 		
-		System.out.println(c.getArea());
-		System.out.println(c.getPerimeter());
+		// Cirkel
+		System.out.println("Cirkel Areal" + c.getArea());
+		System.out.println("Cirkel Perimeter " + c.getPerimeter());
+		// Ellipse
+		System.out.println("Ellipse Areal "+ e.getArea());
+		System.out.println("Ellipse Perimeter" + e.getPerimeter());
+		// Trapez
+		System.out.println("Trapez Areal " + t.getArea());
+		System.out.println("Trapez Perimeter " + t.getPerimeter());
+		// 
+		
+		
 	}
 
 }
